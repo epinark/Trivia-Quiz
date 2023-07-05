9313cb7d7728f4e72a57ea0002982af13c0f440c
epinark <122348695+epinark@users.noreply.github.com>
2023-07-05 11:46:39 +0200

    Responsive Design
    added responsive design and applied shadows to buttons

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -10,26 +10,20 @@ export default function Question({ question, currentIndex, setCurrentIndex, sele
     };
 
     return (
-        <div className="flex flex-col">
+        <div className="flex flex-col justify-center text-center items-center">
             <p className="p-2 m-2">Question {currentIndex + 1}</p>
             <p>{question.question}</p>
-            <div className="flex flex-row gap-5 p-2 m-2 self-center">
+            <div className="flex flex-col sm:flex-row justify-center text-center gap-5 p-2 m-2 self-center items-center">
                 {question.answers.map((answer, index) => (
                     <div key={index}>
                         <button
-                            className="border rounded p-2 m-2"
-                            style={{
-                                backgroundColor:
-                                    selectedAnswers[question.id] &&
-                                        selectedAnswers[question.id].answer === answer
-                                        ? 'yellow' : '',
-                            }}
+                            className={`flex flex-row max-w-md items-center border rounded p-2 m-2 shadow-sm ${selectedAnswers[question.id] &&
+                                selectedAnswers[question.id].answer === answer
+                                ? 'bg-yellow-300'
+                                : ''
+                                }`}
                             onClick={() =>
-                                handleClick(
-                                    question.id,
-                                    answer,
-                                    answer === question.correctAnswer
-                                )
+                                handleClick(question.id, answer, answer === question.correctAnswer)
                             }
                         >
                             {answer}
@@ -39,18 +33,15 @@ export default function Question({ question, currentIndex, setCurrentIndex, sele
             </div>
             {currentIndex < length - 1 && (
                 <button
-                    className='rounded cursor-pointer border self-center p-2 m-2 bg-green'
-                    style={{
-                        backgroundColor:
-                            selectedAnswers[question.id]
-                                ? 'lightgreen' : '',
-                    }}
+                    className={`rounded cursor-pointer border self-center p-2 m-2 shadow-sm ${selectedAnswers[question.id] ? 'bg-green-600' : ''
+                        }`}
                     disabled={!selectedAnswers[question.id]}
                     onClick={() => setCurrentIndex((prev) => prev + 1)}
                 >
                     Next
                 </button>
-            )}
-        </div>
+            )
+            }
+        </div >
     );
 }
