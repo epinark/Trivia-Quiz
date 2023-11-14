b72b69dbd8fade7ac422811358aa34b12e076e27
epinark <122348695+epinark@users.noreply.github.com>
2023-11-14 16:28:13 +0100

    add loading on start-page

--- src/components/StartAnimation.jsx
+++ src/components/StartAnimation.jsx
@@ -1,15 +1,8 @@
-import start from "../assets/start.svg";
-
 const StartAnimation = () => {
   return (
     <div>
       <div className="flex flex-col justify-center items-center h-screen">
         <div className="border-t-4 border-gray-500 rounded-full animate-spin h-14 w-14"></div>
-
-        <div className="h-16 w-16">
-          <img src={start} alt="start" />
-        </div>
-
         <span className="ml-3 pt-4 text-gray-500 text-lg">Loading...</span>
       </div>
     </div>
